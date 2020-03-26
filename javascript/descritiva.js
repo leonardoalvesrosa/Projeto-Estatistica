245c045c23e326627660dd4586b349b7f92a6b76
leonardoalvesrosa <alvesrosaleonardo14@gmail.com>
2020-03-25 23:15:35 -0300

    Update descritiva.js

--- javascript/descritiva.js
+++ javascript/descritiva.js
@@ -1,12 +1,24 @@
 
 const nomevar = document.getElementById('nomevar')
 const dadosvar = document.getElementById('dados')
-const salve = document.getElementById('salv')
+const salve = document.getElementById('calc')
 
 
 
 
-function salvar (){ 
+
+
+
+
+
+
+function criaTag(elemento){
+    return document.createElement(elemento)
+}
+
+function visuTabela(){
+
+
     let dados = []
     let dadosSep = []
     let nome = nomevar.value 
@@ -21,36 +33,86 @@ function salvar (){
     dados.push(conteudo)
     dadosSep = dados.toString().split(';')
     console.log(dadosSep)
-    
-}
+   
+    let tabela = document.getElementById('tabela')
+    //let nometitulo = document.getElementById('nomecol')
 
-salve.addEventListener('click', salvar) 
 
-function visuTabela(nome, dados){
+    let thead = criaTag("thead")
+    let tbody = criaTag("tbody")
+    let tfoot = criaTag("tfoot")
 
-    var tb = document.getElementById('tabela')
-    var nometitulo = document.getElementById('nomecol')
-    var qtdlinha = tb.rows.length
-    var linha = tb.insertRow(qtdlinha)
+    tabela.appendChild(thead)
+    tabela.appendChild(tbody)
+    tabela.appendChild(tfoot)
 
-    
-    var cell = tb.insertRow(qtdlinha)
-    var celldados = linha.insertCell(0)
- 
-    nometitulo.innerHTML = nome
-  
+    let indiceTabela = [nome, "Frequência", "Frequência (%)", "Frequência Acumulada", "FAC (%)"]
 
-    dadosSep = dados.toString().split(';')
-    console.log(dadosSep)
-    for(let item in dadosSep){
-       celldados.innerHTML += dadosSep[item] + '<br>'
+
+    let linhaHead = criaTag("tr")
+
+    function criaCelula(tag, text) {
+        tag = criaTag(tag)
+        tag.textContent = text
+        return tag
     }
     
+
+    for(let i = 0; i <= indiceTabela.length -1 ; i++){
+        let th = criaCelula("th" , indiceTabela[i])
+        linhaHead.appendChild(th)
+    }
+    
+    thead.appendChild(linhaHead)
+
+    //dadosSep = dados.toString().split(';')
+
+
+    for(let j = 0; j < dadosSep.length; j++){
+        
+        let linhaBody = criaTag("tr")
+        //linhaBody.appendChild(criaCelula("td" ))
+        let cell = criaCelula("td", dadosSep[j])
+        linhaBody.appendChild(cell)
+        
+        tbody.appendChild(linhaBody)
+    }
+
+    
+
+
+
+
+
+    /*
+    for(let j = 0; j < dadosSep.length; j++){
+        //console.log(dadosSep[j])
+        
+        let linhaBody = criaTag("tr")
+        linhaBody.appendChild(criaCelula("td", (j+1)))
+        for(let i = 0; i < dadosSep[j].length; i++){
+            //console.log(dadosSep[j][i])
+            let cell = criaCelula("td", dadosSep[j][i])
+            linhaBody.appendChild(cell)
+        }
+        tbody.appendChild(linhaBody)
+    }
+    
+*/
+
+
+    let linhaFoot = criaTag("tr")
+    let celulaFoot = criaCelula("td", "Rodape")
+    celulaFoot.setAttribute("colspan",3)
+    
+    linhaFoot.appendChild(celulaFoot)
+    tbody.tfoot.appendChild(linhaFoot)
+    
     
     
 }
 
-console.log(dadosSep.length)
+
 
 
 
